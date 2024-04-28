c108fa7a57997b41ec1a08a8469f0510e353be43
tingru0515 <tingru0515@gmail.com>
2024-04-28 10:54:58 +1000

    q4

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,14 @@ wait 1 second then yield a random number.
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """This coroutine will loop 10 times,
+    each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """This coroutine will execute async_comprehension
+    four times in parallel using asyncio.gather
+    """
     start_time = asyncio.get_event_loop().time()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = asyncio.get_event_loop().time()
