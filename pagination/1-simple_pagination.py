eab72dad8f6e86e5d0af4d33c400ad9ffcdab2f7
tingru0515 <tingru0515@gmail.com>
2024-04-30 19:10:22 +1000

    q1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""This module defines index_range function and Server class"""
 import csv
 import math
 from typing import List
@@ -11,6 +12,7 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -32,9 +34,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Retrieve a specific page of data from the dataset."""
-            start_index, end_index = index_range(page, page_size)
-            dataset = self.dataset()
-            if start_index >= len(dataset):
-                 return []
-            return dataset[start_index:end_index]
+        """Retrieve a specific page of data from the dataset."""
+        assert (isinstance(page, int) and isinstance(page_size, int)
+                and page > 0 and page_size > 0)
+        if self.__dataset is None:
+            self.dataset()
+        pagination: Tuple[int, int] = index_range(page, page_size)
+        return [each_page
+                for each_page in self.__dataset[pagination[0]:pagination[1]]]
+    
